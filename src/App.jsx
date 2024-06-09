3d71c442256aba13a5474fa724c528ca0d9ef7df
Sayli29 <sayli.tawhare@gmail.com>
2024-06-09 18:49:02 +0530

    feat: updated the responsive part of calculator.

--- src/App.jsx
+++ src/App.jsx
@@ -27,11 +27,11 @@ function App() {
 
   return (
     <>
-      <div className="flex flex-col justify-center items-center gap-12 w-screen h-screen bg-slate-100 px-80 py-16 max-sm:p-0">
-        <h1 className='text-slate-700 font-mono text-4xl max-sm:hidden'>{AppLevelConstants.CALCULATOR_HEADING}</h1>
+      <div className="flex flex-col justify-center items-center gap-12 w-screen h-screen bg-slate-100 px-80 py-16 max-md:p-0 max-xl:px-40">
+        <h1 className='text-slate-700 font-mono text-4xl max-md:hidden'>{AppLevelConstants.CALCULATOR_HEADING}</h1>
         <div className='flex flex-col gap-4 text-black bg-white w-full h-full rounded dark:text-white dark:bg-slate-900'>
           <div className='w-full h-1/4 p-8'>Input</div>
-          <div className='grid grid-cols-4 gap-2 w-full h-full bg-slate-50 rounded-t-2xl p-8 dark:text-white dark:bg-slate-800 max-sm:gap-y-8 gap-x-4'>
+          <div className='grid grid-cols-4 gap-2 w-full h-full bg-slate-50 rounded-t-2xl p-8 dark:text-white dark:bg-slate-800 max-[375px]:gap-y-8 gap-x-4'>
           <button className="btn btn-operation" onClick={toggleMode}>
             { isDarkMode ? <DarkMode></DarkMode> :<LightMode></LightMode>}
           </button>
